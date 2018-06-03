227b577679c95ff7352edb1a3ef027d2d2d4e1ee
Pavithra Kodmad <pksjce@gmail.com>
2018-06-03 21:06:46 +1000

    Needs hack for injectGlobals to update

--- src/Utils/global-styles.js
+++ src/Utils/global-styles.js
@@ -5,84 +5,88 @@ import { Children } from 'react'
 import { withTheme, injectGlobal } from 'styled-components'
 
 const Global = ({ theme, children }) => {
-  injectGlobal`
-  @import url('https://fonts.googleapis.com/css?family=Montserrat:300,400,700');
-  body {
-    margin: 0;
-    padding: 0;
-    font-family: Montserrat, Arial, sans-serif;
-    font-size: ${remcalc(14)};
-    color: #666;
-    letter-spacing: ${remcalc(0.11)};
-    line-height: ${remcalc(21)};
-    padding-bottom: ${remcalc(40)};
-    background-color: ${theme.primary};
-  }
-
-  div[id*='do-not-delete-this-hack'] {
-    display: none;
-  }
-
-  a {
-    color: ${theme.blue};
-    text-decoration: none;
-    padding-bottom: ${remcalc(2)};
-    border-bottom: ${remcalc(2)} solid ${theme.blue};
-    position: relative;
-    padding: ${remcalc(5)};
-    opacity: 0.8;
-    font-wheight: 300;
-    letter-spacing: ${remcalc(0.09)};
-    text-align: left;
-    line-height: ${remcalc(21)};;
-    cursor: pointer;
-    transition: color 200ms ease;
-
-    &.no-hover {
-        line-height: 1.8;
-        &:hover {
-            color: ${theme.blue};
+    injectGlobal`
+        @import url('https://fonts.googleapis.com/css?family=Montserrat:300,400,700');
+        body {
+          margin: 0;
+          padding: 0;
+          font-family: Montserrat, Arial, sans-serif;
+          font-size: ${remcalc(14)};
+          color: #666;
+          letter-spacing: ${remcalc(0.11)};
+          line-height: ${remcalc(21)};
+          padding-bottom: ${remcalc(40)};
+          background-color: ${theme.primary};
         }
-        &:after {
-            display: none;
+
+        div[id*='do-not-delete-this-hack'] {
+          display: none;
         }
-    }
 
-    span {
-      position: relative;
-      z-index: 10;
-    }
+        a {
+          color: ${theme.blue};
+          text-decoration: none;
+          padding-bottom: ${remcalc(2)};
+          border-bottom: ${remcalc(2)} solid ${theme.blue};
+          position: relative;
+          padding: ${remcalc(5)};
+          opacity: 0.8;
+          font-wheight: 300;
+          letter-spacing: ${remcalc(0.09)};
+          text-align: left;
+          line-height: ${remcalc(21)};;
+          cursor: pointer;
+          transition: color 200ms ease;
 
-    @media (pointer: fine) {
-      &:after {
-        transition: height 200ms ease;
-        content: '';
-        width: 100%;
-        height: 0;
-        background: ${theme.blue};
-        display: block;
-        position: absolute;
-        bottom: 0;
-        z-index: 0;
-      }
+          &.no-hover {
+              line-height: 1.8;
+              &:hover {
+                  color: ${theme.blue};
+              }
+              &:after {
+                  display: none;
+              }
+          }
 
-      &:hover {
-        color: ${theme.primary};
+          span {
+            position: relative;
+            z-index: 10;
+          }
 
-        &:after {
-          height: 100%;
+          @media (pointer: fine) {
+            &:after {
+              transition: height 200ms ease;
+              content: '';
+              width: 100%;
+              height: 0;
+              background: ${theme.blue};
+              display: block;
+              position: absolute;
+              bottom: 0;
+              z-index: 0;
+            }
+
+            &:hover {
+              color: ${theme.primary};
+
+              &:after {
+                height: 100%;
+              }
+            }
+          }
         }
-      }
-    }
-  }
 
-  ul {
-    padding: 0;
-    margin: 0;
-    list-style: none;
-  }
-`
-  return Children.only(children)
+        ul {
+          padding: 0;
+          margin: 0;
+          list-style: none;
+        }
+        /* HACK! I'm using the table tag so this does not affect any styles. This should work till https://github.com/styled-components/styled-components/pull/1493 gets merged*/
+        table {
+          border-image-width: ${Math.random()};
+        }
+      `
+    return Children.only(children)
 }
 
 export default withTheme(Global)
